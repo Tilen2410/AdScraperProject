9e7b3b8aa03ec78a1cc20ff96962d9271558f689
Tilen2410 <tilen.zitek@gmail.com>
2023-10-16 21:22:53 +0200

    changed css selector

--- scraper/adscraper/adscraper/spiders/adspider.py
+++ scraper/adscraper/adscraper/spiders/adspider.py
@@ -14,7 +14,7 @@ class AdspiderSpider(scrapy.Spider):
 
     def start_requests(self):
         url = "https://www.sreality.cz/en/search/for-sale/apartments"
-        yield scrapy.Request(url, meta={'playwright': True})
+        yield scrapy.Request(url, meta={'playwright': True, 'download_timeout': 0})
 
 
     def parse(self, response):
@@ -34,20 +34,19 @@ class AdspiderSpider(scrapy.Spider):
             """
             apartment_page_url = base_url + apartment_page
             self.last_page_visited = apartment_page_url
-            yield response.follow(apartment_page_url, callback=self.parse_apartment_page, meta={'playwright': True})
+            yield response.follow(apartment_page_url, callback=self.parse_apartment_page, meta={'playwright': True, 'download_timeout': 0})
         
         next_page = response.xpath("//ul[@class='paging-full']/li[@class='paging-item']/following-sibling::li[11]/a/@href").get()
         #print(f"naslednja stran:{next_page}")
         next_page_url = base_url + next_page
         if self.num_ads < self.item_limit:
-            yield response.follow(next_page_url, callback=self.parse, meta={'playwright': True})
+            yield response.follow(next_page_url, callback=self.parse, meta={'playwright': True, 'download_timeout': 0})
 
 
     def parse_apartment_page(self, response):
         title1 = response.css('.property-title h1 span.name.ng-binding::text').get()
-        #title2 = response.xpath("//div[@class='property-title']/h1/span/span[contains(@class, 'name') and contains(@class, 'ng-binding')]/text()").get()         btn-paging-pn icof icon-arr-right paging-next
-        image1 = response.css('.ng-isolate-scope .ob-c-carousel__content img::attr(src)').get()
-        #image2 = response.xpath("//div[@class='ng-isolate-scope']/div[@class='ob-c-carousel__content']/div[1]/img/@src").get()
+        #image1 = response.css('.ng-isolate-scope .ob-c-carousel__content img::attr(src)').get()
+        image1 = response.css('.ob-c-carousel__content img::attr(src)').get()
         """
         yield {
             'title': title1,
